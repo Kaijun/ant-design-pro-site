6109fbe5425739f8652ddb97641462b7ad9cbf64
nikogu <644506165@qq.com>
2017-10-18 16:14:57 +0800

    Updates

--- docs/i18n.md.js
+++ docs/i18n.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([26,34],{1693:function(e,t){e.exports={content:["article",["p","\u5f85\u8865\u5145\u3002"]],meta:{order:12,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",filename:"docs/i18n.md"},toc:["ul"]}}});
+webpackJsonp([33,41],{1624:function(e,t){e.exports={content:["article",["p","\u5f85\u8865\u5145\u3002"]],meta:{order:12,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",filename:"docs/i18n.md"},toc:["ul"]}}});
