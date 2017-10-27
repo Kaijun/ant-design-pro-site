f5bba217b6869cf74e0876ee8f293f08ff56ef9f
nikogu <644506165@qq.com>
2017-10-27 15:35:30 +0800

    Updates

--- docs/history.md.js
+++ docs/history.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([40,47],{1508:function(t,e){t.exports={content:["article",["h3","0.1.0"],["p","\u7b2c\u4e00\u4e2a\u7248\u672c\u3002"]],meta:{order:20,title:"\u66f4\u65b0\u65e5\u5fd7",type:"\u5176\u4ed6",filename:"docs/history.md"},toc:["ul"]}}});
+webpackJsonp([40,47],{1522:function(t,e){t.exports={content:["article",["h3","0.1.0"],["p","\u7b2c\u4e00\u4e2a\u7248\u672c\u3002"]],meta:{order:20,title:"\u66f4\u65b0\u65e5\u5fd7",type:"\u5176\u4ed6",filename:"docs/history.md"},toc:["ul"]}}});
--- docs/i18n.md.js
+++ docs/i18n.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([39,47],{1509:function(e,t){e.exports={content:["article",["p","\u5f85\u8865\u5145\u3002"]],meta:{order:12,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",filename:"docs/i18n.md"},toc:["ul"]}}});
+webpackJsonp([39,47],{1523:function(e,t){e.exports={content:["article",["p","\u5f85\u8865\u5145\u3002"]],meta:{order:12,title:"\u56fd\u9645\u5316",type:"\u8fdb\u9636",filename:"docs/i18n.md"},toc:["ul"]}}});
